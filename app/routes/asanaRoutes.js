c08a5ecb120377ca72d840d997f62d60d3964f04
Don Mamaril <mamarildon@gmail.com>
2014-06-16 22:34:21 -0700

    Set up /user/update path

--- app/routes/asanaRoutes.js
+++ app/routes/asanaRoutes.js
@@ -11,7 +11,6 @@ module.exports = function (app) {
   /* === FETCH SPECIFIC USER & GRAB TASKS === */
   app.get('/users', function (req, res) {
     User.findOne({ _id: req.user._id }, function (err, user) {
-
       var options     = {};
       options.method  = 'GET';
       options.url     = asanaURL + '/workspaces/1213745087037/projects'
@@ -21,14 +20,12 @@ module.exports = function (app) {
 
       request(options, function (err, response, projects) {
         projects = JSON.parse(projects).data;
-        
         projects.forEach(function (project) {
-          if (project.name === 'Amira Anuar') { // replace user.asana.name
-            
+          if (project.name === 'Amira Anuar') { // replace user.asana.name  
             user.projectId = project.id;
             user.save();
-
             options.url = asanaURL + '/projects/' + project.id + '/tasks';
+
             request(options, function (err, response, tasks) {
               if (!user.progress.length) { utils.saveProgress(JSON.parse(tasks).data, user); }
               res.send(JSON.parse(tasks).data);
@@ -45,6 +42,17 @@ module.exports = function (app) {
     User.findOne({ _id: req.user._id }, function (err, user) {
       if (err) { throw err; }
 
+      // req.body = {
+      //   taskId  : current task to be moved
+      //   header  : header it transferred to { name: headerName, id: id }
+      //   from    : header it came from { name: fromName : id: id }
+      // }
+
+      var options = {};
+      options.method  = 'POST';
+      options.url     = asanaURL + '/tasks/' + req.body.taskId + '/addProject';
+      options.headers = { 'Authorization' : 'Bearer ' + user.asana.token }
+
       var options = {
         method      : 'POST',
         url         : 'https://app.asana.com/api/1.0/tasks/' + req.body.taskId + '/addProject',
@@ -55,9 +63,20 @@ module.exports = function (app) {
         headers     : { 'Authorization' : 'Bearer ' + user.asana.token }
       };
 
-      request(moveoptions, function(err,httpResponse,body) {
-        // do stuff body = { 'data' : {} } on success
-      });
+      // .indexOf WILL NOT WORK IT IS AN ARRAY OF OBJECTS
+      var from = user.progress.indexOf(req.body.from.name);
+      var to   = user.progress.indexOf(req.body.header.name);
+
+      for (var i = from+1 ; i <= to ; i++) {
+        options.form = {
+          'project'      : user.projectId,
+          'insert_after' : user.progress[i].id
+        }
+
+        request(options, function (err, httpResponse, body) {
+          // do stuff body = { 'data' : {} } on success
+        });
+      }
     });
   });
 };
