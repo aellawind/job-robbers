e84bb05dfb711eb0f274f7b6591f66ef4c498afc
Don Mamaril <mamarildon@gmail.com>
2014-06-18 11:16:26 -0700

    fix bug that resent emails every 2 mins

--- app/routes/mailGun.js
+++ app/routes/mailGun.js
@@ -61,7 +61,7 @@ module.exports = function (app) {
         };
 
         request(moveoptions, function (err, httpResponse, body) {
-          console.log(body);
+          res.send(200);
         });
 
       });
