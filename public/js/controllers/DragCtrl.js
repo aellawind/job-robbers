4e1a55e6c2d9551e755df025ea5f926e2368e354
Jimmy Tsao <jimmy.tsao@gmail.com>
2014-06-18 23:32:51 -0700

    Added functionality to prevent dragging backwards

--- public/js/controllers/DragCtrl.js
+++ public/js/controllers/DragCtrl.js
@@ -1,8 +1,23 @@
-app.controller('DragController', function ($scope, $modal, Students) {
+app.controller('DragController', function ($scope, $modal, Students, statusOrder) {
 
   Students.fetchTasks()
     .then(function(data){
+      var obj;
+      
+      for (var i = 0; i<data.length; i++){
+        obj = data[i];
+        if (statusOrder[obj.header]){
+          obj.statusOrder = statusOrder[obj.header];
+        } else {
+          obj.statusOrder = undefined;
+        }
+      }
+
+      data.sort(function(a,b){
+        return a.statusOrder - b.statusOrder;
+      });
       $scope.data = data;
+      console.log($scope.data);
     });
 
   $scope.renderModal = function (task) {
