083ad130e1977a4342d784c01ddad74d64629c95
Amira Anuar <amiraanuar@gmail.com>
2014-06-21 12:18:59 -0700

    Change asana api keys

--- config/asanaKeys.js
+++ config/asanaKeys.js
@@ -1,15 +1,17 @@
 module.exports = {
 
- // 'asanaAuth': {
- //  'clientID'     : process.env.CLIENTID,
- //  'clientSecret' : process.env.CLIENTSECRET,
- //  'callbackURL'  : process.env.CALLBACK
- // }
+ 'asanaAuth': {
+  'clientID'     : process.env.CLIENTID,
+  'clientSecret' : process.env.CLIENTSECRET,
+  'callbackURL'  : process.env.CALLBACK
+ }
 
- 'asanaAuth' : {
-    'clientID'      : '13235029827133',
-    'clientSecret'  : '829a75907ceff9ac6ce709d45f774c88',
-    'callbackURL'   : 'http://127.0.0.1:8080/auth/asana/callback'
-  },
+ // 'asanaAuth' : {
+ //    'clientID'      : '13235029827133',
+ //    'clientSecret'  : '829a75907ceff9ac6ce709d45f774c88',
+ //    'callbackURL'   : 'http://127.0.0.1:8080/auth/asana/callback'
+ //  },
 
 };
+
+/libs/bootstrap-css-only/css/bootstrap.min.css
