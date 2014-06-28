66c2f252beffc5fb123e0dab461a71f8608e0831
Don Mamaril <mamarildon@gmail.com>
2014-06-28 11:41:45 -0700

    clean keys

--- config/asanaKeys.js
+++ config/asanaKeys.js
@@ -1,15 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
   'asanaAuth': {
     'clientID'     : process.env.CLIENTID || '13235029827133',
     'clientSecret' : process.env.CLIENTSECRET || '829a75907ceff9ac6ce709d45f774c88',
     'callbackURL'  : process.env.CALLBACK || 'http://127.0.0.1:8080/auth/asana/callback'
-=======
-  
-  'asanaAuth' : {
-    'clientID'      : '13235029827133',
-    'clientSecret'  : '829a75907ceff9ac6ce709d45f774c88',
-    'callbackURL'   : 'http://127.0.0.1:8080/auth/asana/callback'
->>>>>>>  switch to ui router
   }
 };
